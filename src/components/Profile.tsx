940a9454908602d3411182d8ca3e219643ea5be0
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2021-03-10 14:01:38 -0300

    send my project in github

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://avatars.githubusercontent.com/u/68797494?s=460&u=9447525dd660ebec378f4b3e4c3751739314efe4&v=4" alt="Tiago Augusto" />
+            <div>
+                <strong>Tiago Augusto</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level {level}
+                </p>
+            </div>
+        </div>
+    );
+}
